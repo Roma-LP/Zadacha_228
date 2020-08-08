4b0a8eb9f8de34e7613087d8dd4db5083985af1e
roma-poit <rlepesh@mail.ru>
2020-08-08 17:03:34 +0300

    Fin

--- Zadacha_228/Program.cs
+++ Zadacha_228/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,23 @@ namespace Zadacha_228
     {
         static void Main(string[] args)
         {
+            ArrayList AY = new ArrayList() {1,6,9,10 };
+            ArrayList BE = new ArrayList() {2,3,5,11,12 };
+
+            // перебор значений
+            foreach (object o in AY)
+            {
+                Console.Write(o+"\t");
+            }
+            Console.WriteLine();
+            // перебор значений
+            foreach (object o in BE)
+            {
+                Console.Write(o + "\t");
+            }
+            Console.Clear();
+            Test.FunkOne(AY, BE);
+            
         }
     }
 }
---
+++ Zadacha_228/Test.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha_228
+{
+    class Test
+    {
+        public static void FunkOne(ArrayList A, ArrayList B)
+        {
+            int index = 0;
+            for (int i = 0; i < B.Count; i++)
+            {
+                if ((int)B[i] > (int)A[index])  // проверяем каждое новое цисло В[] из А[]
+                {
+                    while (true)
+                    {
+                        if ((int)B[i] > (int)A[index])
+                        {
+                            if (A.Count == 1+index)
+                            {
+                                A.Add(B[i]);
+                                break;
+                            }
+                            else
+                            {
+                                index++;
+                            }
+                        }
+                        else
+                        {
+                            A.Insert(index, B[i]);
+                            index++;
+                            break;
+                        }
+                    }
+
+                }
+                else
+                {
+                    A.Insert(index, B[i]);
+                    index++;
+                }
+            }
+
+            // перебор значений
+            foreach (object o in A)
+            {
+                Console.Write(o + "\t");
+            }
+            Console.WriteLine();
+            // перебор значений
+            foreach (object o in B)
+            {
+                Console.Write(o + "\t");
+            }
+
+        }
+    }
+}
